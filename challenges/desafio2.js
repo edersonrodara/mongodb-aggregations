4e63c8087447716df363dc0086fd720cc4f04aa1
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-08-19 17:25:09 -0300

    desafio2 - feito

---
+++ challenges/desafio2.js
@@ -0,0 +1,24 @@
+db.movies.aggregate(
+  [
+    {
+      $match: {
+        $and: [
+          { "imdb.rating": { $gte: 7 } },
+          { genres: { $nin: ["Crime", "Horror"] } },
+          { $or: [{ rated: "PG" }, { rated: "G" }] },
+          { languages: { $all: ["English", "Spanish"] } },
+        ],
+      },
+    },
+    {
+      $project: {
+        _id: false,
+        titulo: "$title",
+        avaliado: "$rated",
+        notaIMDB: "$imdb.rating",
+        votosIMDB: "$imdb.votes",
+        ano: "$year",
+      },
+    },
+  ],
+);
